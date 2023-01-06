2a8770f1c9eeabccf57e9eb8f3c24d91eff93c2a
Loki99911 <nikita_kresik@i.ua>
2023-01-06 14:16:06 +0200

    10

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,47 @@
-import { HomeTitle } from './Home.styled';
-
+import { useSelector } from 'react-redux';
+import {
+  HomeContainer,
+  HomeTitle,
+  HomeContainerLogin,
+  Link,
+} from './Home.styled';
+import { SlActionRedo} from 'react-icons/sl';
+import { BsEmojiFrown, BsEmojiWink } from 'react-icons/bs';
 const Home = () => {
+  const isLogin = useSelector(state => state.user.isLogin);
   return (
-    <div>
+    <HomeContainer>
       <HomeTitle>Welcome to Your phone book!!! </HomeTitle>
-    </div>
+      {isLogin ? (
+        <HomeContainerLogin>
+          <p>
+            Ok, you entered.
+            <BsEmojiWink />
+          </p>
+          <p>Let's get to work:</p>
+          <Link to="/contacts">
+            <SlActionRedo />
+            Contacts
+          </Link>
+        </HomeContainerLogin>
+      ) : (
+        <HomeContainerLogin>
+          <p>
+            You don`t register yet...
+            <BsEmojiFrown />
+          </p>
+          <p>Let's get log in or register:</p>
+          <Link to="/register">
+            <SlActionRedo />
+            Register
+          </Link>
+          <Link to="/login">
+            <SlActionRedo />
+            Login
+          </Link>
+        </HomeContainerLogin>
+      )}
+    </HomeContainer>
   );
 };
 
--- src/components/Home/Home.styled.jsx
+++ src/components/Home/Home.styled.jsx
@@ -1,10 +1,44 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const HomeContainer = styled.div`
+  font-size: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 30px;
+`;
 
 export const HomeTitle = styled.h1`
   font-size: 36px;
+  font-family: 'Caveat', cursive;
   display: flex;
   align-items: center;
   justify-content: center;
   margin: 0 auto;
   margin-top: 30px;
+`;
+
+export const HomeContainerLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const Link = styled(NavLink)`
+  display: inline-block;
+  padding: 8px 10px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: #35e6d6;
+  font-weight: 500;
+  border: 1px solid #35e6d6;
+  &.active {
+    color: black;
+    background-color: #35e6d6;
+  }
 `;
